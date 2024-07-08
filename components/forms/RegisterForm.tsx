4197d359657fd89430b9df061002b34ff92c7929
Rishav Kumar <rishavkr1766@gmail.com>
2024-07-08 14:11:09 +0530

    start medical information section

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -6,6 +6,7 @@ import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import {
   Form,
+  FormControl,
   
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
@@ -16,6 +17,11 @@ import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
 import { createUser } from "@/lib/actions/patient.actions"
 import { FormFieldType } from "./PatientForm"
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
+import { Doctors, GenderOptions } from "@/constants"
+import { Label } from "../ui/label"
+import { SelectItem } from "../ui/select"
+import Image from "next/image"
 
 
 
@@ -109,14 +115,94 @@ const  RegisterForm = ({user}:{user: User})=> {
       />
 
 <CustomFormField 
-      fieldType={FormFieldType.PHONE_INPUT}
+      fieldType={FormFieldType.SKELETON}
       control={form.control}
-      name="phone"
-      label="Phone Number"
-      placeholder="+123 456 7890"
+      name="gender"
+      label="Gender"
+      renderSkeleton={(field)=>(
+        <FormControl>
+          <RadioGroup className="flex h-11 gap-6 xl:justify-between" onValueChange={field.onChange} defaultValue={field.value}>
+            {GenderOptions.map((option)=>(
+              <div key={option} className="radio-group">
+                <RadioGroupItem value={option} id={option}/>
+                <Label htmlFor={option} className="cursor-pointer">
+                  {option}
+                </Label>
+              </div>
+            ))}
+          </RadioGroup>
+        </FormControl>
+      )}
       
       />
     </div>
+
+
+      <div className="flex flex-col gap-6 xl:flex-row">
+
+      <CustomFormField 
+      fieldType={FormFieldType.INPUT}
+      control={form.control}
+      name="address"
+      label="Address"
+      placeholder="14th Street, New York"
+      
+      />
+
+        <CustomFormField 
+        fieldType={FormFieldType.INPUT}
+        control={form.control}
+        name="occupation"
+        label="Occupation"
+        placeholder="Software developer"
+        />
+
+      </div>
+
+      <div className="flex flex-col gap-6 xl:flex-row">
+
+    <CustomFormField 
+      fieldType={FormFieldType.INPUT}
+      control={form.control}
+      name="emergencyContactName"
+      label="Emergency contact name"
+      placeholder="Guardian's name"
+      
+      />
+
+    <CustomFormField 
+      fieldType={FormFieldType.PHONE_INPUT}
+      control={form.control}
+      name="emergencyContactNumber"
+      label="Emergency contact number"
+      placeholder="+123 456 7890"
+      
+      />
+      </div>
+
+      <section className="space-y-6">
+        <div className="mb-9 space-y-1">
+        <h2 className="sub-header">Medical Information</h2>
+        </div>
+      </section>
+
+      <CustomFormField 
+      fieldType={FormFieldType.SELECT}
+      control={form.control}
+      name="primaryPhysician"
+      label="Primary Physician"
+      placeholder="Select your primary physician"
+      
+      >
+        {Doctors.map((doctor)=>(
+          <SelectItem key={doctor.name} value={doctor.name}>
+            <div className="flex cursor-pointer items-center gap-2">
+              <Image src={doctor.image} width={32} height={32} alt={doctor.name} className="rounded-full border border-dark-500"/>
+              <p>{doctor.name}</p>
+            </div>
+          </SelectItem>
+        ))}
+      </CustomFormField>
 
       <SubmitButton isLoading={isLoading}>
         Get Started
