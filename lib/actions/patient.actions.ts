b1fc1c11b33c3c827270823d893dcb5b5119c876
Rishav Kumar <rishavkr1766@gmail.com>
2024-07-09 16:38:19 +0530

    feat: Add getPatient function to patient actions

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -65,3 +65,17 @@ export const createUser = async (user: CreateUserParams) => {
     }
   }
 
+  export const getPatient = async (userId: string) => {
+    try{
+      const patients = await databases.listDocuments(
+        DATABASE_ID!,
+        PATIENT_COLLECTION_ID!,
+        [
+          Query.equal('userId', userId)
+        ]
+      );
+      return parseStringify(patients.documents[0]);
+    } catch (error){
+      console.error("An error occurred while fetching user:", error);
+    }
+  }
